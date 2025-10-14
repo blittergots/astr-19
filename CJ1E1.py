214e332105b129d7cd4be91315ad32d7e55cb20f
Lux <lux.morgan@gmail.com>
2025-10-14 11:37:51 -0700

    Code Journal 1 Prompt 1
    An introduction to me. Name Pronouns, Favorite food and movies.

---
+++ CJ1E1.py
@@ -0,0 +1,24 @@
+#This program is prompt 1 of CODE JOURNAL #1
+
+#this function tells the computer to print my journal entry.
+
+def CJ1E1 ():
+	#What's my name?
+	print ('My full name is Lux Morgan.')
+	#What are my pronouns?
+	print ('My pronouns are He/Him')
+	#What's my favorite food?
+	print ('My favorite foods are anything with fish. I particularly enjoy sushi, poke bowls, and fish tacos.')
+	#What's my favorite movie(s)?
+	print ('My favorite movies are How to Train Your Dragon, Ghost in the Shell, Everything Everywhere all at Once, and Perfect Blue.')
+
+def main():
+	CJ1E1()
+
+
+def another_function():
+	print('something else')
+
+
+if __name__=="__main__":
+	main()
